551903c3c821e997398cdd2980729d4fdd842989
mathieu <mathieu128bb9@gmail.com>
2025-11-09 10:48:51 +0000

    ajout ra, rb, rr

--- push_swap/rotate.c
+++ push_swap/rotate.c
@@ -6,7 +6,7 @@
 /*   By: msuter <msuter@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 20:55:56 by msuter            #+#    #+#             */
-/*   Updated: 2025/11/08 21:31:35 by msuter           ###   ########.fr       */
+/*   Updated: 2025/11/09 10:48:09 by msuter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,41 @@ void	rotate_a(int *tab_a, int *size_a)
 	int	i;
 
 	i = 0;
-	temp = tab_a[*size_a - 1];
-	while (i < *size_a - 1)				// FAUXXXXXXXXXXXXXXXXXXX recommencer
+	if (*size_a < 2)
+		return ;
+	temp = tab_a[0];
+	while (i < *size_a - 1)
 	{
 		tab_a[i] = tab_a[i + 1];
 		i++;
 	}
-	tab_a[0] = temp;
+	tab_a[*size_a - 1] = temp;
+	ft_printf("ra\n");
+}
+
+void	rotate_b(int *tab_b, int *size_b)
+{
+	int	temp;
+	int	i;
+
+	i = 0;
+	if (size_b < 2)
+		return ;
+	temp = tab_b[0];
+	while (i < *size_b - 1)
+	{
+		tab_b[i] = tab_b[i + 1];
+		i++;
+	}
+	tab_b[*size_b - 1] = temp;
+	ft_printf("rb\n");
+}
+
+void	rotate_both(int *tab_a, int *tab_b, int size_a, int size_b)
+{
+	if (size_a >= 2)
+		rotate_a(*tab_a, size_a);
+	if (size_b >= 2)
+		rotate_b(*tab_b, size_b);
+	ft_printf("rr\n");
 }
