2f543c2d3ec29875da778a96584e9e49cb11fb55
mathieu <mathieu128bb9@gmail.com>
2025-11-24 17:45:17 +0000

    petit coup de norminette

--- push_swap/is_in_lis.c
+++ push_swap/is_in_lis.c
@@ -6,11 +6,25 @@
 /*   By: msuter <msuter@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 10:03:14 by msuter            #+#    #+#             */
-/*   Updated: 2025/11/24 16:57:35 by msuter           ###   ########.fr       */
+/*   Updated: 2025/11/24 17:40:57 by msuter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
+
+static int	value_in_lis(int value, int *lis, int size_lis)
+{
+	int	j;
+
+	j = 0;
+	while (j < size_lis)
+	{
+		if (value == lis[j])
+			return (1);
+		j++;
+	}
+	return (0);
+}
 
 void	is_in_lis(int *tab_a, int *size_a, int *tab_b, int *size_b)
 {
@@ -18,7 +32,6 @@ void	is_in_lis(int *tab_a, int *size_a, int *tab_b, int *size_b)
 	int	size_lis;
 	int	orig_size;
 	int	i;
-	int	j;
 	int	in_lis;
 
 	lis = NULL;
@@ -29,14 +42,7 @@ void	is_in_lis(int *tab_a, int *size_a, int *tab_b, int *size_b)
 	i = 0;
 	while (i < orig_size)
 	{
-		in_lis = 0;
-		j = 0;
-		while (j < size_lis)
-		{
-			if (tab_a[0] == lis[j])
-				in_lis = 1;
-			j++;
-		}
+		in_lis = value_in_lis(tab_a[0], lis, size_lis);
 		if (in_lis)
 			rotate_a(tab_a, size_a);
 		else
