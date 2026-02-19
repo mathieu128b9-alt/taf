74a25ac41e7f140fa8ac5fd9602457ae23bf0c55
mathieu <mathieu128bb9@gmail.com>
2026-02-19 22:01:24 +0100

    j'ai modif les deux fonctions quotes, pour gerer les cas particuliers faudra probablement revoir une partie de la logique, je pense que faut malloc dans la fonction centrale et faire en sorte de boucler tant que on tombe pas sur un operateur ou un espace, comme ca un big malloc et c'est good. autre solution, gerer au cas par cas chaque quotes, doubles etc, et faire un strjoin a la fin, a voir le plus renta et logique.

--- minishell/all_lexer/each_case_for_lexing.c
+++ minishell/all_lexer/each_case_for_lexing.c
@@ -6,69 +6,56 @@
 /*   By: msuter <msuter@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:07:14 by msuter            #+#    #+#             */
-/*   Updated: 2026/02/19 14:53:43 by msuter           ###   ########.fr       */
+/*   Updated: 2026/02/19 21:58:03 by msuter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	case_single_quote(char *imput, t_token *token, int *i, int *nb)
+int	case_single_quote(char *imput, t_token *token, int *i, int *size_word)
 {
-	int	temp;
-	int	size_word;
+	int temp;
 
 	temp = *i + 1;
-	size_word = 1;
 	while (imput[temp] && imput[temp] != '\'')
 	{
 		size_word++;
 		temp++;
 		if (imput[temp] == '\'')
-		{
-			(token[*nb]).content = malloc(sizeof(char) * size_word + 2);
-			if (!(token[*nb]).content)
-				case_error(imput, token, "erreur lors du malloc d'un token");
-			ft_strlcpy((token[*nb].content), imput + *i, size_word + 2);
-			(*i) = (*i) + size_word + 1;
-			return ((*nb)++);
-		}
+			return (size_word + 1);
 	}
 	return (1);
 }
 
-int	case_double_quote(char *imput, t_token *token, int *i, int *nb)
+int	case_double_quote(char *imput, t_token *token, int *i, int *size_word)
 {
-	int	temp;
-	int	size_word;
+	int temp;
 
 	temp = *i + 1;
-	size_word = 1;
 	while (imput[temp] && imput[temp] != '\"')
 	{
 		size_word++;
 		temp++;
 		if (imput[temp] == '\"')
-		{
-			(token[*nb]).content = malloc(sizeof(char) * size_word + 2);
-			if (!(token[*nb]).content)
-				case_error(imput, token, "erreur lors du malloc d'un token");
-			ft_strlcpy((token[*nb].content), imput + *i, size_word + 2);
-			(*i) = (*i) + size_word + 1;
-			return ((*nb)++);
-		}
+			return (size_word + 1);
 	}
 	return (1);
 }
 
 void	case_word(char *imput, t_token *token, int *i, int *nb)
 {
+	int size_word;
+
+	size_word = 0;
 	if (imput[*i] == '\'')
 	{
-		case_single_quote(imput, token, i, nb);
+		size_word++;
+		case_single_quote(imput, token, i, &size_word);
 	}
 	if (imput[*i] == '\"')
 	{
-		
+		size_word++;
+		case_double_quote(imput, token, i, &size_word);
 	}
 	while (imput[*i] && imput[*i] != ' ')
 	{
@@ -77,8 +64,3 @@ void	case_word(char *imput, t_token *token, int *i, int *nb)
 	printf("erreur, il manque une quote");
 	exit(1);
 }
-
-/* 
-	manque a gerer les cas ou je trouve jamais de deuxieme quote,
-	le cas ou ya du texte colle a ma deuxieme qute(pris en compte)
-*/
