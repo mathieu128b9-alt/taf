71d59f1c952ace5731229595eaec242681d546ce
msuter <mathieusuter@yahoo.com>
2026-03-02 12:37:00 +0100

    la gestion commande + arg a l'air d'etre potentiellement bien, mtn faut que je l'apelle correctement dans mon parser, et faire quelques tests

--- minishell/all_parser/parser.c
+++ minishell/all_parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: msuter <msuter@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 18:19:27 by msuter            #+#    #+#             */
-/*   Updated: 2026/03/02 11:35:38 by msuter           ###   ########.fr       */
+/*   Updated: 2026/03/02 12:32:44 by msuter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,29 @@
 
 void	arg_after_cmd(t_token *token, t_parser *parser, int *nb)
 {
-	
+	int	temp;
+	int	count;
+
+	count = 0;
+	temp = *nb;
+	while (token[temp].type == TOKEN_WORD)
+	{
+		temp++;
+		count++;
+	}
+	parser->arg = malloc(sizeof(char *) * (count + 2));
+	if (!parser->arg)
+		return ;
+	count = 0;
+	parser->arg[count] = ft_strdup(parser->cmd);
+	count++;
+	while(*nb != temp)
+	{
+		parser->arg[count] = ft_strdup(token[*nb].content);
+		count++;
+		(*nb)++;
+	}
+	parser->arg[count] = NULL;
 }
 
 void	cmd_or_file(t_token *token, t_parser *parser, int *nb)
@@ -23,13 +45,15 @@ void	cmd_or_file(t_token *token, t_parser *parser, int *nb)
 	{
 		parser->cmd = ft_strdup(token[*nb].content);
 		(*nb)++;
+		arg_after_cmd(token, parser, nb);
 	}
 	else if (*nb - 1 >= 0)
 	{
-		if (is_redirect(token, (nb - 1)) == 0)
+		if (is_redirect(token, ((*nb) - 1)) == 0)
 		{
 			parser->cmd = ft_strdup(token[*nb].content);
 			(*nb)++;
+			arg_after_cmd(token, parser, nb);
 		}
 	}
 }
